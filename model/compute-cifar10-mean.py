b8438ac5abab901e2583bd13962cea82a6a1f0e3
ofririps <ofririps@u4c3965621bde5a.ant.amazon.com>
2018-09-01 19:07:17 +0300

    don't compress png file

--- model/compute-cifar10-mean.py
+++ model/compute-cifar10-mean.py
@@ -23,9 +23,10 @@ import numpy as np
 
 import cv2
 
-data = dset.CIFAR10(root='cifar', train=True, download=True,
-                    transform=transforms.ToTensor()).train_data
+data = dset.CIFAR10(root='cifar', train=True, download=True, transform=transforms.ToTensor()).train_data
 data = data.astype(np.float32) / 255.
+img = data.mean(axis=0)
+# cv2.imwrite("/home/ANT.AMAZON.COM/ofririps/workspace/PipeCNN-DL/model/mean_data.png", data.mean(axis=0))
 
-cv2.imwrite("/home/ANT.AMAZON.COM/ofririps/workspace/PipeCNN-DL/model/mean_data.png",
-	        data.mean(axis=0))
+cv2.imwrite("/home/ANT.AMAZON.COM/ofririps/workspace/PipeCNN-DL/model/mean_data.png", img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
