2b7eeaf08f494987c6ca3f82e7b397fdfcddb69c
ofririps <ofririps@u4c3965621bde5a.ant.amazon.com>
2018-09-01 19:10:25 +0300

    don't normalize mean data

--- model/compute-cifar10-mean.py
+++ model/compute-cifar10-mean.py
@@ -24,7 +24,7 @@ import numpy as np
 import cv2
 
 data = dset.CIFAR10(root='cifar', train=True, download=True, transform=transforms.ToTensor()).train_data
-data = data.astype(np.float32) / 255.
+data = data.astype(np.float32)
 img = data.mean(axis=0)
 # cv2.imwrite("/home/ANT.AMAZON.COM/ofririps/workspace/PipeCNN-DL/model/mean_data.png", data.mean(axis=0))
 
