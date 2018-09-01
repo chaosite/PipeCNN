0742554db3aa6c2e714820a9779617f045757c01
ofririps <ofririps@u4c3965621bde5a.ant.amazon.com>
2018-09-01 18:26:58 +0300

    cifar10 mean data as png

---
+++ model/compute-cifar10-mean.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import torch
+
+import torch.nn as nn
+import torch.optim as optim
+
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+import torchvision.models as models
+
+import os
+import sys
+import math
+
+import numpy as np
+
+import cv2
+
+data = dset.CIFAR10(root='cifar', train=True, download=True,
+                    transform=transforms.ToTensor()).train_data
+data = data.astype(np.float32) / 255.
+
+cv2.imwrite("/home/ANT.AMAZON.COM/ofririps/workspace/PipeCNN-DL/model/mean_data.png",
+	        data.mean(axis=0))
